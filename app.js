8f466d9103618cc10e6e7cce182d5a218fee02f6
brentexam <brent-21@hotmail.ca>
2015-03-26 17:34:23 -0400

    COMMITETED

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,14 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-// db 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/lesson6');
-
-// tell the application which route / controller to use for product requests
-var products = require('./routes/products');
-app.use('/', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,7 +55,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 
 module.exports = app;
